efc2d45764447a6d050c394c6c06f29caee89a0a
Fabio Di Cecca <fdi-cecc@c4a3c9.42berlin.de>
2024-06-21 16:07:18 +0200

    windows and hooks set

--- main.c
+++ main.c
@@ -6,45 +6,59 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 17:05:57 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/06/21 11:28:31 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/06/21 16:03:20 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "minilibx_macos/mlx.h"
 #include "minilibx_linux/mlx.h"
+#include <X11/keysym.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#define MALLOC_ERROR 1
+#define MLX_ERROR 1
 #define HEIGHT 200
 #define WIDTH 200
 
+typedef struct	s_mlx_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}				t_mlx_data;
+
+
+int	hook_react(int keysim, t_mlx_data *data)
+{
+	if (keysim == XK_Escape)
+	{
+		printf("The %d (ESC) has been pressed\n\n", keysim);
+		mlx_clear_window(data->mlx_ptr, data->win_ptr);
+		mlx_destroy_display(data->mlx_ptr);
+		free(data->mlx_ptr);
+		exit (1);
+	}
+	printf("The %d has been pressed\n\n", keysim);
+	return (0);
+}
+
 int	main ()
 {
-	void	*mlx_connection;
-	void	*mlx_window1;
-	void	*mlx_window2;
+	t_mlx_data data;
 
-	mlx_connection = mlx_init();
-	if (NULL == mlx_connection)
-		return (MALLOC_ERROR);
-	mlx_window1 = mlx_new_window(mlx_connection, HEIGHT, WIDTH, "WindowTest 1");
-	if (NULL == mlx_window1)
+	data.mlx_ptr = mlx_init();
+	if (NULL == data.mlx_ptr)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, HEIGHT, WIDTH, "WindowTest 1");
+	if (NULL == data.win_ptr)
 	{
-		mlx_destroy_window(mlx_connection, mlx_window1);
-		free(mlx_connection);
-		return (MALLOC_ERROR);
+		mlx_destroy_window(data.mlx_ptr, data.win_ptr);
+		free(data.mlx_ptr);
+		return (MLX_ERROR);
 	}
-	mlx_window1 = mlx_new_window(mlx_connection, HEIGHT, WIDTH, "WindowTest 2");
-	if (NULL == mlx_window2)
-	{
-		mlx_destroy_window(mlx_connection, mlx_window2);
-		free(mlx_connection);
-		return (MALLOC_ERROR);
-	}
-	mlx_loop(mlx_connection);
-	mlx_destroy_window(mlx_connection, mlx_window1);
-	mlx_destroy_window(mlx_connection, mlx_window2);
-	free(mlx_connection);
+	mlx_key_hook(data.win_ptr, hook_react, &data);
+	mlx_loop(data.mlx_ptr);
+	free(data.mlx_ptr);
+	return (0);
 }
 
 //cc main.c -Iminilibx_macos -Lminilibx_macos -lmlx -framework OpenGL -framework AppKit
