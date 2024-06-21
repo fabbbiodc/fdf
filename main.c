8bbf6e49a33ecd642b3b5f298bbb29fcaf7a16eb
Fabio Di Cecca <fdi-cecc@c4a3c9.42berlin.de>
2024-06-21 14:35:29 +0200

    tests

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 17:05:57 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/06/20 17:52:43 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/06/21 11:28:31 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx_macos/mlx.h"
+//#include "minilibx_macos/mlx.h"
+#include "minilibx_linux/mlx.h"
 #include <stdlib.h>
 
 #define MALLOC_ERROR 1
@@ -19,22 +20,32 @@
 
 int	main ()
 {
-	void *mlx_connection;
-	void *mlx_window;
+	void	*mlx_connection;
+	void	*mlx_window1;
+	void	*mlx_window2;
 
 	mlx_connection = mlx_init();
 	if (NULL == mlx_connection)
 		return (MALLOC_ERROR);
-	mlx_window = mlx_new_window(mlx_connection, HEIGHT, WIDTH, "WindowTest");
-	if (NULL == mlx_window)
+	mlx_window1 = mlx_new_window(mlx_connection, HEIGHT, WIDTH, "WindowTest 1");
+	if (NULL == mlx_window1)
 	{
-		mlx_destroy_window(mlx_connection, mlx_window);
+		mlx_destroy_window(mlx_connection, mlx_window1);
+		free(mlx_connection);
+		return (MALLOC_ERROR);
+	}
+	mlx_window1 = mlx_new_window(mlx_connection, HEIGHT, WIDTH, "WindowTest 2");
+	if (NULL == mlx_window2)
+	{
+		mlx_destroy_window(mlx_connection, mlx_window2);
 		free(mlx_connection);
 		return (MALLOC_ERROR);
 	}
 	mlx_loop(mlx_connection);
-	mlx_destroy_window(mlx_connection, mlx_window);
+	mlx_destroy_window(mlx_connection, mlx_window1);
+	mlx_destroy_window(mlx_connection, mlx_window2);
 	free(mlx_connection);
 }
 
 //cc main.c -Iminilibx_macos -Lminilibx_macos -lmlx -framework OpenGL -framework AppKit
+//cc main.c -Iminilibx_linux -Lminilibx_linux -lmlx -lX11 -lXext
