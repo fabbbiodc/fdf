78c57a4184096ae412f6456fc125dff5e885b036
Fabio Di Cecca <fdi-cecc@c4b2c6.42berlin.de>
2024-06-24 18:33:20 +0200

    pixel attempt

--- mainmlx42.c
+++ mainmlx42.c
@@ -6,7 +6,7 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 16:09:22 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/06/23 17:53:39 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/06/24 18:17:26 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 #define MLX_ERROR 1
 #define HEIGHT 200
 #define WIDTH 200
+
+static mlx_image_t* image;
 
 void	ft_hook(mlx_key_data_t keydata, void* param)
 {
@@ -37,11 +39,29 @@ int	main()
 	if(!(window = mlx_init(WIDTH, HEIGHT, "testone", true)))
 	{
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
+
+/* 	if (!(image = mlx_new_image(window, 100, 100)))
+	{
+		mlx_close_window(window);
+		puts(mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
+	}
+
+	if (mlx_image_to_window(window, image, 10, 10) == -1)
+	{
+		mlx_close_window(window);
+		puts(mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
+	}
+	 */
+	mlx_put_pixel(window, 20, 20, 0xFFFF0000);
 	
 	mlx_key_hook(window, ft_hook, window);
 	mlx_loop(window);
+	
+	//mlx_close_window(window);
 	mlx_terminate(window);
 	return (0);
 }
